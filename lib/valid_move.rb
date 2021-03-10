ecc2504928aeab8cce12121db0382be67e1cafd5
Melissa Vogelgesang <mvogelgesang01@gmail.com>
2021-03-10 21:04:13 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,5 +1,5 @@
 # code your #valid_move? method here
-def valid_move(board, position)
+def valid_move?(board, position)
   spot = position.to_i-1
   if !position_taken?(board, spot) && spot.between?(0,8)
     true
